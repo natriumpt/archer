6664ca8527309aadb87185a7745b07487509c19e
Alexandre Ramos <natrium.pt@gmail.com>
2017-03-15 17:01:17 +0000

    Initial commit, project setup

---
+++ desktop/src/org/academiadecodigo/hackathon/archer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.hackathon.archer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.academiadecodigo.hackathon.archer.ArcherGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ArcherGame(), config);
+	}
+}
