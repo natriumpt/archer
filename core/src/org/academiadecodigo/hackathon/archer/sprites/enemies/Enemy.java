9473cbe036a219a24950a536191790834ed474bd
Peter <oliveirap1990@gmail.com>
2017-03-16 21:34:10 +0000

    Skeleton and Enemy 1st implementation

--- core/src/org/academiadecodigo/hackathon/archer/sprites/enemies/Enemy.java
+++ core/src/org/academiadecodigo/hackathon/archer/sprites/enemies/Enemy.java
@@ -1,6 +1,56 @@
 package org.academiadecodigo.hackathon.archer.sprites.enemies;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.World;
+import org.academiadecodigo.hackathon.archer.screens.GameScreen;
 
 public abstract class Enemy extends Sprite {
+
+
+    private World world;
+    private Body body;
+    private GameScreen gameScreen;
+    private Vector2 velocity;
+    private int health;
+
+
+
+    public Enemy(GameScreen screen, float initial_x, float initial_y){
+
+        this.world = screen.getWorld();
+        this.gameScreen = screen;
+        setPosition(initial_x, initial_y);
+        defineEnemy();
+        velocity = new Vector2(-1f, -2f);
+        body.setActive(false);
+    }
+
+    protected abstract void defineEnemy();
+
+    public abstract void update(float dt);
+
+    public abstract void hitByEnemy(Enemy enemy);
+
+    void setBody(Body body){
+        this.body = body;
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    World getWorld(){
+        return world;
+    }
+
+
+//    public void reverseVelocity(boolean x, boolean y){
+//        if(x)
+//            velocity.x = -velocity.x;
+//        if(y)
+//            velocity.y = -velocity.y;
+//    }
+
 }
--- core/src/org/academiadecodigo/hackathon/archer/sprites/enemies/Skeleton.java
+++ core/src/org/academiadecodigo/hackathon/archer/sprites/enemies/Skeleton.java
@@ -1,4 +1,58 @@
 package org.academiadecodigo.hackathon.archer.sprites.enemies;
 
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import org.academiadecodigo.hackathon.archer.ArcherGame;
+import org.academiadecodigo.hackathon.archer.screens.GameScreen;
+import org.academiadecodigo.hackathon.archer.sprites.projectile.Projectile;
+
+import java.util.ArrayList;
+
 public class Skeleton extends Enemy {
+
+    public static final int MAX_ACTIVE_PROJECTILES = 10;
+
+    private ArrayList<Projectile> projectiles;
+
+    public Skeleton(GameScreen screen, float initial_x, float initial_y) {
+        super(screen, initial_x, initial_y);
+    }
+
+    @Override
+    protected void defineEnemy() {
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(200, 200);
+
+        setBody(getWorld().createBody(bodyDef));
+
+        CircleShape shape = new CircleShape();
+        shape.setRadius(400 / ArcherGame.PPM);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+//        fixtureDef.density = 1f;
+//        fixtureDef.friction = 0.4f;
+//        fixtureDef.restitution = 0.6f;
+        getBody().createFixture(fixtureDef);
+
+//        shape.dispose();
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void hitByEnemy(Enemy enemy) {
+
+    }
+
+    public void fire(){
+        
+    }
 }
