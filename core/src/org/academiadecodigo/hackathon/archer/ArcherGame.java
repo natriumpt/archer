fb12dddfcb44e70cd6caf3d19c4e59f22f90b580
filipe campos <camplaix@gmail.com>
2017-03-17 07:17:09 +0000

    prepare audio folder assets

--- core/src/org/academiadecodigo/hackathon/archer/ArcherGame.java
+++ core/src/org/academiadecodigo/hackathon/archer/ArcherGame.java
@@ -1,12 +1,14 @@
 package org.academiadecodigo.hackathon.archer;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import org.academiadecodigo.hackathon.archer.screens.GameScreen;
 
 public class ArcherGame extends Game {
 
 	public SpriteBatch batch;
+    public static AssetManager manager;
 
     //Virtual Screen size and Box2D Scale(Pixels Per Meter)
     public static final int V_WIDTH = 400;
@@ -18,6 +20,8 @@ public class ArcherGame extends Game {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+        manager = new AssetManager();
+        manager.load("audio/music/");
 		setScreen(new GameScreen((this)));
 	}
 
